33cafb12e44eb3c547cfb7928ba340f453ac4361
leogregianin@gmail.com <leogregianin@gmail.com>
2017-08-31 17:33:30 -0400

    added pypi

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='bancocentralbrasil',
+      version='1.0',
+      description='Selic Rate, Inflation and Dollar Exchange from Brazil',
+      author='Leonardo Gregianin',
+      author_email='leogregianin@gmail.com',
+      url='https://github.com/leogregianin/bancocentralbrasil',
+     )
