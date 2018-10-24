92c49a632360fc1201740d21e5d2d6ae4329d0c6
Diego Ponciano <dgo.ponciano@gmail.com>
2018-10-24 12:24:42 -0300

    Adds tests for requests retry

--- test_bancocentral.py
+++ test_bancocentral.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+from unittest.mock import patch, MagicMock
+from bancocentral import AcessarBancoCentral
 from bancocentral import Inflacao, Poupanca, Cambio, Selic
+
 
 class TestCase(unittest.TestCase):
 
@@ -11,6 +14,31 @@ class TestCase(unittest.TestCase):
         self.poupanca = Poupanca()
         self.cambio = Cambio()
         self.selic = Selic()
+
+    """ Retry """
+    @patch('bancocentral.requests.get')
+    def test_nao_retorna_request_com_erro(self, mock_request):
+        mock_request.return_value.status_code = 503
+
+        acesso = AcessarBancoCentral('http://my.url')
+        self.assertIsNone(acesso.getURL())
+
+    @patch('bancocentral.requests.get')
+    def test_retorna_request_sem_erro(self, mock_request):
+        mock_request.return_value.status_code = 200
+
+        acesso = AcessarBancoCentral('http://my.url')
+        self.assertIsNotNone(acesso.getURL())
+
+    @patch('bancocentral.requests.get')
+    def test_repete_request_enquanto_receber_erros(self, mock_request):
+        retorno_com_erro = MagicMock(status_code=503)
+        retorno_sem_erro = MagicMock(status_code=200)
+        retornos = iter([retorno_com_erro, retorno_sem_erro])
+        mock_request.side_effect = lambda *args, **kwargs: next(retornos)
+
+        acesso = AcessarBancoCentral('http://my.url')
+        self.assertIsNotNone(acesso.getURL())
 
     """ Inflação """
     def test_inflacao_meta(self):
@@ -96,6 +124,7 @@ class TestCase(unittest.TestCase):
 
     def test_selic_real_maior_zero(self):
         self.assertTrue(self.selic.get_selic_real() > 0)
+
 
 if __name__ == '__main__':
     unittest.main()
