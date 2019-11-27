7c7d076911f980ea234e6634f36f3b9a2bc0eb97
leogregianin <leogregianin@gmail.com>
2019-11-27 15:17:08 -0400

    fix pylint problems

--- bancocentral.py
+++ bancocentral.py
@@ -26,7 +26,7 @@ class AcessarBancoCentral:
         }
 
         # Server very unstable. Test 10x http 200 response
-        for index in range(10):
+        for _ in range(10):
             try:
                 request = requests.get(self.url, headers=headers, timeout=None)
                 if request.status_code == 200:
@@ -54,12 +54,12 @@ class Inflacao:
 
     def get_meta_tax(self):
         inflacao = cleanContent(self.req.content.decode('utf-8'))
-        tax = re.search('<div id=label>Meta</div><div id=rate><div id=value>(\d*\,?\d+)</div>', inflacao).group(1)
+        tax = re.search(r'<div id=label>Meta</div><div id=rate><div id=value>(\d*\,?\d+)</div>', inflacao).group(1)
         return float(tax.replace(',','.'))
 
     def get_acumulada_tax(self):
         inflacao = cleanContent(self.req.content.decode('utf-8'))
-        tax = re.search('<div id=label>Acumulada</div><div id=rate><div id=value>(\d*\,?\d+)</div>', inflacao).group(1)
+        tax = re.search(r'<div id=label>Acumulada</div><div id=rate><div id=value>(\d*\,?\d+)</div>', inflacao).group(1)
         return float(tax.replace(',','.'))
 
 
@@ -72,7 +72,7 @@ class Poupanca:
 
     def get_poupanca_tax(self):
         poupanca = cleanContent(self.req.content.decode('utf-8'))
-        tax = re.search('<div id=value>(\d*\,?\d+)</div>', poupanca).group(1)
+        tax = re.search(r'<div id=value>(\d*\,?\d+)</div>', poupanca).group(1)
         return float(tax.replace(',','.'))
 
 
@@ -85,43 +85,43 @@ class Cambio:
         self.cambio = cleanContent(self.req.content.decode('utf-8'))
 
     def get_dolar_compra_ptax(self):
-        data = re.search('INDICADOR_CAMBIO_DOLAR_PTAX(.*)</entry>', self.cambio).group(1)
-        compra = re.search('<div id=rate><div id=label>Compra</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
+        data = re.search(r'INDICADOR_CAMBIO_DOLAR_PTAX(.*)</entry>', self.cambio).group(1)
+        compra = re.search(r'<div id=rate><div id=label>Compra</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
         return float(compra.replace(',','.'))
 
     def get_dolar_venda_ptax(self):
-        data = re.search('INDICADOR_CAMBIO_DOLAR_PTAX(.*)</entry>', self.cambio).group(1)
-        venda = re.search('<div id=rate><div id=label>Venda</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
+        data = re.search(r'INDICADOR_CAMBIO_DOLAR_PTAX(.*)</entry>', self.cambio).group(1)
+        venda = re.search(r'<div id=rate><div id=label>Venda</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
         return float(venda.replace(',','.'))
 
     def get_dolar_compra(self):
-        data = re.search('INDICADOR_CAMBIO_DOLAR(.*)</entry>', self.cambio).group(1)
-        compra = re.search('<div id=rate><div id=label>Compra</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
+        data = re.search(r'INDICADOR_CAMBIO_DOLAR(.*)</entry>', self.cambio).group(1)
+        compra = re.search(r'<div id=rate><div id=label>Compra</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
         return float(compra.replace(',','.'))
 
     def get_dolar_venda(self):
-        data = re.search('INDICADOR_CAMBIO_DOLAR(.*)</entry>', self.cambio).group(1)
-        venda = re.search('<div id=rate><div id=label>Venda</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
+        data = re.search(r'INDICADOR_CAMBIO_DOLAR(.*)</entry>', self.cambio).group(1)
+        venda = re.search(r'<div id=rate><div id=label>Venda</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
         return float(venda.replace(',','.'))
 
     def get_euro_compra_ptax(self):
-        data = re.search('INDICADOR_CAMBIO_EURO_PTAX(.*)</entry>', self.cambio).group(1)
-        compra = re.search('<div id=rate><div id=label>Compra</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
+        data = re.search(r'INDICADOR_CAMBIO_EURO_PTAX(.*)</entry>', self.cambio).group(1)
+        compra = re.search(r'<div id=rate><div id=label>Compra</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
         return float(compra.replace(',','.'))
 
     def get_euro_venda_ptax(self):
-        data = re.search('INDICADOR_CAMBIO_EURO_PTAX(.*)</entry>', self.cambio).group(1)
-        venda = re.search('<div id=rate><div id=label>Venda</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
+        data = re.search(r'INDICADOR_CAMBIO_EURO_PTAX(.*)</entry>', self.cambio).group(1)
+        venda = re.search(r'<div id=rate><div id=label>Venda</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
         return float(venda.replace(',','.'))
 
     def get_euro_compra(self):
-        data = re.search('INDICADOR_CAMBIO_EURO(.*)</entry>', self.cambio).group(1)
-        compra = re.search('<div id=rate><div id=label>Compra</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
+        data = re.search(r'INDICADOR_CAMBIO_EURO(.*)</entry>', self.cambio).group(1)
+        compra = re.search(r'<div id=rate><div id=label>Compra</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
         return float(compra.replace(',','.'))
 
     def get_euro_venda(self):
-        data = re.search('INDICADOR_CAMBIO_EURO(.*)</entry>', self.cambio).group(1)
-        venda = re.search('<div id=rate><div id=label>Venda</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
+        data = re.search(r'INDICADOR_CAMBIO_EURO(.*)</entry>', self.cambio).group(1)
+        venda = re.search(r'<div id=rate><div id=label>Venda</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
         return float(venda.replace(',','.'))
 
 
@@ -134,10 +134,10 @@ class Selic:
 
     def get_selic_meta(self):
         selic = cleanContent(self.req.content.decode('utf-8'))
-        selic_meta = re.search('<div id=ratevalue>(\d*\,?\d+)</div>', selic).group(1)
+        selic_meta = re.search(r'<div id=ratevalue>(\d*\,?\d+)</div>', selic).group(1)
         return float(selic_meta.replace(',','.'))
 
     def get_selic_real(self):
         selic = cleanContent(self.req.content.decode('utf-8'))
-        selic_real = re.search('<div id=dailyratevalue>(\d*\,?\d+)</div>', selic).group(1)
+        selic_real = re.search(r'<div id=dailyratevalue>(\d*\,?\d+)</div>', selic).group(1)
         return float(selic_real.replace(',','.'))
