d74317961e90ec24b779b931a061f0c058dfd20f
Leonardo Gregianin <leogregianin@gmail.com>
2021-03-11 06:55:05 -0400

    Revert "Correção de issue e aprimoramentos."

--- bc/bancocentral.py
+++ bc/bancocentral.py
@@ -54,26 +54,12 @@ class Inflacao:
 
     def get_meta_tax(self):
         inflacao = cleanContent(self.req.content.decode('utf-8'))
-        if not inflacao:
-          return None
-        try:
-          tax = re.search(r'<div id=label>Meta</div><div id=rate><div id=value>(\d*[\.\,]?\d+)</div>', inflacao).group(1)
-        except AttributeError:
-          return None
-        except:
-          raise
+        tax = re.search(r'<div id=label>Meta</div><div id=rate><div id=value>(\d*\,?\d+)</div>', inflacao).group(1)
         return float(tax.replace(',','.'))
 
     def get_acumulada_tax(self):
         inflacao = cleanContent(self.req.content.decode('utf-8'))
-        if not inflacao:
-          return None
-        try:
-          tax = re.search(r'<div id=label>Acumulada</div><div id=rate><div id=value>(\d*[\.\,]?\d+)</div>', inflacao).group(1)
-        except AttributeError:
-          return None
-        except:
-          raise
+        tax = re.search(r'<div id=label>Acumulada</div><div id=rate><div id=value>(\d*\,?\d+)</div>', inflacao).group(1)
         return float(tax.replace(',','.'))
 
 
@@ -86,14 +72,7 @@ class Poupanca:
 
     def get_poupanca_tax(self):
         poupanca = cleanContent(self.req.content.decode('utf-8'))
-        if not poupanca:
-          return None
-        try:
-          tax = re.search(r'<div id=value>(\d*[\.\,]?\d+)</div>', poupanca).group(1)
-        except AttributeError:
-          return None
-        except:
-          raise
+        tax = re.search(r'<div id=value>(\d*\,?\d+)</div>', poupanca).group(1)
         return float(tax.replace(',','.'))
 
 
@@ -107,151 +86,43 @@ class Cambio:
 
     def get_dolar_compra_ptax(self):
         data = re.search(r'INDICADOR_CAMBIO_DOLAR_PTAX(.*)</entry>', self.cambio).group(1)
-        if not data:
-          return None
-        try:
-          compra = re.search(r'<div id=rate><div id=label>Compra</div><div id=value>(\d*[\.\,]?\d+)</div>', data).group(1)
-        except AttributeError:
-          return None
-        except:
-          raise
+        compra = re.search(r'<div id=rate><div id=label>Compra</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
         return float(compra.replace(',','.'))
 
     def get_dolar_venda_ptax(self):
         data = re.search(r'INDICADOR_CAMBIO_DOLAR_PTAX(.*)</entry>', self.cambio).group(1)
-        if not data:
-          return None
-        try:
-          venda = re.search(r'<div id=rate><div id=label>Venda</div><div id=value>(\d*[\.\,]?\d+)</div>', data).group(1)
-        except AttributeError:
-          return None
-        except:
-          raise
+        venda = re.search(r'<div id=rate><div id=label>Venda</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
         return float(venda.replace(',','.'))
-
-    def get_data_dolar_ptax(self):
-        # A data da cotacao eh a mesma para compra e venda
-        data = re.search(r'INDICADOR_CAMBIO_DOLAR_PTAX(.*)</entry>', self.cambio).group(1)
-        if not data:
-          return None
-        try:
-          data_dolar_ptax = re.search(r'<div id=data>[a-zA-Z\s]*([\d/]+\s[\d:]+)</div>', data).group(1)
-        except AttributeError:
-          return None
-        except:
-          raise
-        return data_dolar_ptax
 
     def get_dolar_compra(self):
         data = re.search(r'INDICADOR_CAMBIO_DOLAR(.*)</entry>', self.cambio).group(1)
-        if not data:
-          return None
-        try:
-          compra = re.search(r'<div id=rate><div id=label>Compra</div><div id=value>(\d*[\.\,]?\d+)</div>', data).group(1)
-        except AttributeError:
-          return None
-        except:
-          raise
+        compra = re.search(r'<div id=rate><div id=label>Compra</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
         return float(compra.replace(',','.'))
 
     def get_dolar_venda(self):
         data = re.search(r'INDICADOR_CAMBIO_DOLAR(.*)</entry>', self.cambio).group(1)
-        if not data:
-          return None
-        try:
-          venda = re.search(r'<div id=rate><div id=label>Venda</div><div id=value>(\d*[\.\,]?\d+)</div>', data).group(1)
-        except AttributeError:
-          return None
-        except:
-          raise
+        venda = re.search(r'<div id=rate><div id=label>Venda</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
         return float(venda.replace(',','.'))
-
-    def get_data_dolar(self):
-        # A data da cotacao eh a mesma para compra e venda
-        data = re.search(r'INDICADOR_CAMBIO_DOLAR(.*)</entry>', self.cambio).group(1)
-        if not data:
-          return None
-        try:
-          data_dolar_ptax = re.search(r'<div id=data>[a-zA-Z\s]*([\d/]+\s[\d:]+)</div>', data).group(1)
-        except AttributeError:
-          return None
-        except:
-          raise
-        return data_dolar_ptax
 
     def get_euro_compra_ptax(self):
         data = re.search(r'INDICADOR_CAMBIO_EURO_PTAX(.*)</entry>', self.cambio).group(1)
-        if not data:
-          return None
-        try:
-          compra = re.search(r'<div id=rate><div id=label>Compra</div><div id=value>(\d*[\.\,]?\d+)</div>', data).group(1)
-        except AttributeError:
-          return None
-        except:
-          raise
+        compra = re.search(r'<div id=rate><div id=label>Compra</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
         return float(compra.replace(',','.'))
 
     def get_euro_venda_ptax(self):
         data = re.search(r'INDICADOR_CAMBIO_EURO_PTAX(.*)</entry>', self.cambio).group(1)
-        if not data:
-          return None
-        try:
-          venda = re.search(r'<div id=rate><div id=label>Venda</div><div id=value>(\d*[\.\,]?\d+)</div>', data).group(1)
-        except AttributeError:
-          return None
-        except:
-          raise
+        venda = re.search(r'<div id=rate><div id=label>Venda</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
         return float(venda.replace(',','.'))
-
-    def get_data_euro_ptax(self):
-        # A data da cotacao eh a mesma para compra e venda
-        data = re.search(r'INDICADOR_CAMBIO_EURO_PTAX(.*)</entry>', self.cambio).group(1)
-        if not data:
-          return None
-        try:
-          data_euro_ptax = re.search(r'<div id=data>[a-zA-Z\s]*([\d/]+\s[\d:]+)</div>', data).group(1)
-        except AttributeError:
-          return None
-        except:
-          raise
-        return data_euro_ptax
 
     def get_euro_compra(self):
         data = re.search(r'INDICADOR_CAMBIO_EURO(.*)</entry>', self.cambio).group(1)
-        if not data:
-          return None
-        try:
-          compra = re.search(r'<div id=rate><div id=label>Compra</div><div id=value>(\d*[\.\,]?\d+)</div>', data).group(1)
-        except AttributeError:
-          return None
-        except:
-          raise
+        compra = re.search(r'<div id=rate><div id=label>Compra</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
         return float(compra.replace(',','.'))
 
     def get_euro_venda(self):
         data = re.search(r'INDICADOR_CAMBIO_EURO(.*)</entry>', self.cambio).group(1)
-        if not data:
-          return None
-        try:
-          venda = re.search(r'<div id=rate><div id=label>Venda</div><div id=value>(\d*[\.\,]?\d+)</div>', data).group(1)
-        except AttributeError:
-          return None
-        except:
-          raise
+        venda = re.search(r'<div id=rate><div id=label>Venda</div><div id=value>(\d*\,?\d+)</div>', data).group(1)
         return float(venda.replace(',','.'))
-
-    def get_data_euro(self):
-        # A data da cotacao eh a mesma para compra e venda
-        data = re.search(r'INDICADOR_CAMBIO_EURO(.*)</entry>', self.cambio).group(1)
-        if not data:
-          return None
-        try:
-          data_euro = re.search(r'<div id=data>[a-zA-Z\s]*([\d/]+\s[\d:]+)</div>', data).group(1)
-        except AttributeError:
-          return None
-        except:
-          raise
-        return data_euro
 
 
 class Selic:
@@ -263,48 +134,10 @@ class Selic:
 
     def get_selic_meta(self):
         selic = cleanContent(self.req.content.decode('utf-8'))
-        if not selic:
-          return None
-        try:
-          selic_meta = re.search(r'<div id=ratevalue>(\d*[\.\,]?\d+)</div>', selic).group(1)
-        except AttributeError:
-          return None
-        except:
-          raise
+        selic_meta = re.search(r'<div id=ratevalue>(\d*\,?\d+)</div>', selic).group(1)
         return float(selic_meta.replace(',','.'))
-
-    def get_data_selic_meta(self):
-        selic = cleanContent(self.req.content.decode('utf-8'))
-        if not selic:
-          return None
-        try:
-          data_selic_meta = re.search(r'<div=ratedate>([\d/]+)</div>', selic).group(1)
-        except AttributeError:
-          return None
-        except:
-          raise
-        return data_selic_meta
 
     def get_selic_real(self):
         selic = cleanContent(self.req.content.decode('utf-8'))
-        if not selic:
-          return None
-        try:
-          selic_real = re.search(r'<div id=dailyratevalue>(\d*[\.\,]?\d+)</div>', selic).group(1)
-        except AttributeError:
-          return None
-        except:
-          raise
+        selic_real = re.search(r'<div id=dailyratevalue>(\d*\,?\d+)</div>', selic).group(1)
         return float(selic_real.replace(',','.'))
-    
-    def get_data_selic_real(self):
-        selic = cleanContent(self.req.content.decode('utf-8'))
-        if not selic:
-          return None
-        try:
-          data_selic_real = re.search(r'<div id=dailyratedate>([\d/]+)</div>', selic).group(1)
-        except AttributeError:
-          return None
-        except:
-          raise
-        return data_selic_real
